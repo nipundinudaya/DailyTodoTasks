4c3804313e2a1c59725cd846e03efbadaedc5df9
Nipun Dinudaya <nipundinudaya2003@gmail.com>
2024-12-06 06:08:06 +0530

    Add files via upload

---
+++ DailyTodoTasks.py
@@ -0,0 +1,37 @@
+
+
+
+prioritize = input("enter your Work:")
+priority_value = input("enter your priority value(H/L/M):")
+spend_vary = input("How do you want to Spend Your Time?(Busy/relax/chill):")
+
+
+
+if priority_value =="H" and spend_vary == "Busy":
+    print("Your Priority is High and You have to FOCUS Your work 💎🔒"
+          "and if you want to busy then, you can work every hour🔒")
+if priority_value =="H" and spend_vary == "chill":
+    print("Your Priority is High and You have to FOCUS Your work 💎🔒"
+          "and if you want to chill then, you can spend few times chill with your parents or friends 😜😜😜")
+if priority_value =="H" and spend_vary == "relax":
+    print("Your Priority is High and You have to FOCUS Your work 💎🔒"
+          "and if you want to relax then, you can work while relax in few minutes 😋😋")
+if priority_value =="M" and spend_vary == "Busy":
+    print("Your Priority is Medium and You can reduce for your works"
+          "and if you want to busy then, you can work every hour🔒")
+if priority_value =="M" and spend_vary == "relax":
+    print("Your Priority is Medium and You can reduce for your works"
+          "and if you want to relax then, you can be relax while you are working 👻")
+if priority_value =="M" and spend_vary == "chill":
+    print("Your Priority is Medium and You can reduce for your works"
+          "and if you want to chill then, you can chill while listening music or something")
+if priority_value =="L" and spend_vary == "Busy":
+    print("Your Priority is Low and You can reduce for your works"
+          "and if you want to Busy then, you can have to focus your works💎🔒")
+if priority_value =="L" and spend_vary == "relax":
+    print("Your Priority is Low and You can reduce for your works"
+          "and if you want to relax then, you can do your works relax 😋😋")
+if priority_value =="L" and spend_vary == "chill":
+    print("Your Priority is Low and You can reduce for your works"
+          "and if you want to chill then, you can do your works while chilling 👻")
+
